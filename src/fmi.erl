4e31f4a344b4141b49754c1a96cdb07619e3cb03
pereuna <90834902+pereuna@users.noreply.github.com>
2026-05-06 13:58:08 +0300

    Add hourly FMI weather updates

---
+++ src/fmi.erl
@@ -0,0 +1,181 @@
+%% fmi.erl
+%% Fetch FMI observations and forecasts into the same day directories as entso.xml.
+-module(fmi).
+
+-include_lib("kernel/include/file.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
+
+-export([fetch_day/1, fetch_day/3, fetch_days/1, fetch_today_and_tomorrow/0]).
+
+-define(BASE_URL, "https://opendata.fmi.fi/wfs").
+-define(DEFAULT_PLACE, "Pihlava").
+-define(PX, "/var/www/htdocs/jedi.ydns.eu/var").
+-define(VOL, "/var/www/htdocs/jedi.ydns.eu/volatile").
+-define(LOG, "/var/www/htdocs/jedi.ydns.eu/volatile/fmi.log").
+
+%% Fetch current day and next day using the same start/end values that ENTSO XML contains.
+fetch_today_and_tomorrow() ->
+    fetch_days_from_entso([eutils:today_utc_string(), eutils:tomorrow_utc_string()]).
+
+fetch_day(Day) ->
+    case kurl:fetch_day(Day) of
+        {ok, Metadata} ->
+            fetch_day(Day, first_time(start, Metadata), first_time('end', Metadata));
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+fetch_day(Day, StartUtc0, EndUtc0) ->
+    fetch_day(Day, normalize_utc(StartUtc0), normalize_utc(EndUtc0), place()).
+
+fetch_days(DaySpecs) ->
+    Place = place(),
+    [fetch_day_spec(DaySpec, Place) || DaySpec <- DaySpecs].
+
+fetch_days_from_entso(Days) ->
+    fetch_days([entso_day_spec(Day) || Day <- Days]).
+
+entso_day_spec(Day) ->
+    case kurl:fetch_day(Day) of
+        {ok, Metadata} ->
+            {Day, first_time(start, Metadata), first_time('end', Metadata)};
+        {error, Reason} ->
+            {Day, {error, Reason}}
+    end.
+
+fetch_day_spec({Day, {error, Reason}}, _Place) ->
+    {Day, {error, Reason}};
+fetch_day_spec({Day, StartUtc, EndUtc}, Place) ->
+    {Day, fetch_day(Day, normalize_utc(StartUtc), normalize_utc(EndUtc), Place)}.
+
+fetch_day(Day, StartUtc, EndUtc, Place) ->
+    DayDir = day_dir(Day),
+    ObsOut = filename:join(DayDir, "fmi_obs.xml"),
+    FcOut = filename:join(DayDir, "fmi_fc.xml"),
+    RawOut = filename:join(DayDir, "fmi_raw.tsv"),
+    TmpObs = filename:join(?VOL, ".fmi_obs_" ++ Day ++ ".xml.tmp"),
+    TmpFc = filename:join(?VOL, ".fmi_fc_" ++ Day ++ ".xml.tmp"),
+    TmpRaw = filename:join(?VOL, ".fmi_raw_" ++ Day ++ ".tsv.tmp"),
+    ok = filelib:ensure_dir(filename:join(DayDir, "dummy")),
+    ok = filelib:ensure_dir(filename:join(?VOL, "dummy")),
+    ObsUrl = fmi_url("fmi::observations::weather::timevaluepair", Place, "t2m", StartUtc, EndUtc),
+    FcUrl = fmi_url("fmi::forecast::harmonie::surface::point::timevaluepair", Place, "Temperature", StartUtc, EndUtc),
+    try
+        ok = require_ok(fetch(ObsUrl, TmpObs), {fetch_obs, Day}),
+        ok = require_ok(fetch(FcUrl, TmpFc), {fetch_forecast, Day}),
+        Raw = raw_tsv(TmpObs, TmpFc),
+        ok = file:write_file(TmpRaw, Raw),
+        ok = file:rename(TmpObs, ObsOut),
+        ok = file:rename(TmpFc, FcOut),
+        ok = file:rename(TmpRaw, RawOut),
+        Metadata = #{day => Day, place => Place, start => StartUtc, 'end' => EndUtc,
+            obs_xml => ObsOut, fc_xml => FcOut, raw_tsv => RawOut},
+        log("fmi: päivitetty päivä=~s paikka=~s väli=~s...~s tiedosto=~s", [Day, Place, StartUtc, EndUtc, RawOut]),
+        {ok, Metadata}
+    catch
+        Class:Reason:Stacktrace ->
+            _ = file:delete(TmpObs),
+            _ = file:delete(TmpFc),
+            _ = file:delete(TmpRaw),
+            log("fmi: ERROR päivä=~s paikka=~s väli=~s...~s ~p:~p", [Day, Place, StartUtc, EndUtc, Class, Reason]),
+            logger:debug("fmi stacktrace: ~p", [Stacktrace]),
+            {error, {Class, Reason}}
+    end.
+
+first_time(Key, Metadata) ->
+    case maps:get(Key, Metadata, []) of
+        [Time | _] -> Time;
+        [] -> error({missing_entso_time, Key})
+    end.
+
+normalize_utc(Time) ->
+    S = string:trim(Time),
+    case {length(S), lists:suffix("Z", S)} of
+        {17, true} ->
+            lists:sublist(S, 16) ++ ":00Z";
+        {20, true} ->
+            S;
+        _ ->
+            error({invalid_utc_time, Time})
+    end.
+
+place() ->
+    case os:getenv("FMI_PLACE") of
+        false -> ?DEFAULT_PLACE;
+        "" -> ?DEFAULT_PLACE;
+        Place -> Place
+    end.
+
+day_dir(Day) ->
+    filename:join([?PX | string:split(Day, "-", all)]).
+
+fmi_url(StoredQueryId, Place, Parameters, StartUtc, EndUtc) ->
+    Query = [
+        {"service", "WFS"},
+        {"version", "2.0.0"},
+        {"request", "getFeature"},
+        {"storedquery_id", StoredQueryId},
+        {"place", Place},
+        {"parameters", Parameters},
+        {"starttime", StartUtc},
+        {"endtime", EndUtc}
+    ],
+    ?BASE_URL ++ "?" ++ uri_string:compose_query(Query).
+
+fetch(Url, Out) ->
+    application:ensure_all_started(ssl),
+    application:ensure_all_started(inets),
+    Request = {Url, []},
+    HttpOptions = [{connect_timeout, 10000}, {timeout, 30000}],
+    Options = [{body_format, binary}],
+    case httpc:request(get, Request, HttpOptions, Options) of
+        {ok, {{_, 200, _}, _Headers, Body}} when byte_size(Body) > 0 ->
+            file:write_file(Out, Body);
+        {ok, {{_, 200, _}, _Headers, _Body}} ->
+            {error, empty_body};
+        {ok, {{_, Code, _}, _Headers, _Body}} ->
+            {error, {http_status, Code}};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+require_ok(ok, _Context) ->
+    ok;
+require_ok({error, Reason}, Context) ->
+    error({Context, Reason}).
+
+raw_tsv(ObsXml, FcXml) ->
+    Obs = parse_timevaluepair("havainto", ObsXml),
+    Fc = parse_timevaluepair("ennuste", FcXml),
+    Lines = ["# tyyppi|UTC-aika|lämpötila_C" | Obs ++ Fc],
+    unicode:characters_to_binary([Line ++ "\n" || Line <- Lines]).
+
+parse_timevaluepair(Label, File) ->
+    case file:read_file_info(File) of
+        {ok, #file_info{size = Size}} when Size > 0 ->
+            {Doc, _} = xmerl_scan:file(File),
+            Nodes = xmerl_xpath:string("//*[local-name()='MeasurementTVP']", Doc),
+            [measurement_line(Label, Node) || Node <- Nodes];
+        _ ->
+            error({missing_or_empty_xml, File})
+    end.
+
+measurement_line(Label, Node) ->
+    Time = xpath_string("*[local-name()='time']", Node),
+    Value = xpath_string("*[local-name()='value']", Node),
+    Label ++ "|" ++ Time ++ "|" ++ Value.
+
+xpath_string(Path, Node) ->
+    {_, _, Value} = xmerl_xpath:string("string(" ++ Path ++ ")", Node),
+    unicode:characters_to_list(Value).
+
+log(Format, Args) ->
+    Line = io_lib:format("~s " ++ Format ++ "~n", [timestamp() | Args]),
+    _ = filelib:ensure_dir(?LOG),
+    _ = file:write_file(?LOG, Line, [append]),
+    logger:info(Format, Args),
+    ok.
+
+timestamp() ->
+    {{Y, M, D}, {H, Min, S}} = calendar:local_time(),
+    lists:flatten(io_lib:format("~4..0B-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B", [Y, M, D, H, Min, S])).
--- src/quarter_worker.erl
+++ src/quarter_worker.erl
@@ -6,6 +6,8 @@
 
 -record(entso_xml, {day, file, start = [], 'end' = []}).
 
+-define(FMI_INTERVAL_SECONDS, 60 * 60).
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -15,13 +17,14 @@ do_work() ->
 
 %% gen_server callbacks
 init([]) ->
-    {ok, #{entso_xml => #{}}}.
+    {ok, #{entso_xml => #{}, last_fmi_update => undefined}}.
 
 handle_cast(do_work, State) ->
     TodayUtc = eutils:today_utc_string(),
     FetchDays = fetch_days(TodayUtc, eutils:local_hour()),
     logger:info("qw ~p fetch_days:~p", [calendar:local_time(), FetchDays]),
-    NewState = lists:foldl(fun fetch_and_store/2, State, FetchDays),
+    State1 = lists:foldl(fun fetch_and_store/2, State, FetchDays),
+    NewState = update_fmi_if_due(State1, FetchDays),
     {noreply, NewState};
 
 handle_cast(_Msg, State) ->
@@ -59,3 +62,42 @@ fetch_and_store(Day, State) ->
             logger:error("get_entso_xml: day=~s failed: ~p", [Day, Reason]),
             State
     end.
+
+update_fmi_if_due(State, FetchDays) ->
+    Now = erlang:system_time(second),
+    LastUpdate = maps:get(last_fmi_update, State, undefined),
+    case fmi_update_due(Now, LastUpdate) of
+        true ->
+            update_fmi(State, FetchDays, Now);
+        false ->
+            State
+    end.
+
+update_fmi(State, FetchDays, Now) ->
+    Specs = fmi_day_specs(State, FetchDays),
+    case Specs of
+        [] ->
+            State;
+        _ ->
+            Results = fmi:fetch_days(Specs),
+            logger:info("fmi update results: ~p", [Results]),
+            State#{last_fmi_update => Now, fmi_results => Results}
+    end.
+
+fmi_day_specs(State, FetchDays) ->
+    EntsoByDay = maps:get(entso_xml, State, #{}),
+    lists:filtermap(
+        fun(Day) ->
+            case maps:get(Day, EntsoByDay, undefined) of
+                #entso_xml{start = [Start | _], 'end' = [End | _]} ->
+                    {true, {Day, Start, End}};
+                _ ->
+                    false
+            end
+        end,
+        FetchDays).
+
+fmi_update_due(_Now, undefined) ->
+    true;
+fmi_update_due(Now, LastUpdate) ->
+    Now - LastUpdate >= ?FMI_INTERVAL_SECONDS.
