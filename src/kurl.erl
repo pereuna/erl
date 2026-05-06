96c35e5d4f76e0734a1ff4b03c3e1c06e76dc4bd
pereuna <90834902+pereuna@users.noreply.github.com>
2026-05-06 08:52:38 +0300

    Add ENTSO XML fetch scheduling

--- src/kurl.erl
+++ src/kurl.erl
@@ -1,30 +1,138 @@
 %% karl [YYYY-MM-DD]
 -module(kurl).
--export([getpage/1]).
+
+-include_lib("kernel/include/file.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
+-export([fetch_day/1, getpage/1]).
 
 -define(HOST, "web-api.tp.entsoe.eu").
 -define(DOM, "10YFI-1--------U").
+-define(PX, "/var/www/htdocs/jedi.ydns.eu/var").
+-define(VOL, "/var/www/htdocs/jedi.ydns.eu/volatile").
+-define(LOG, "/var/www/htdocs/jedi.ydns.eu/volatile/entso.log").
 
 apikey() ->
     case os:getenv("ENTSOE_API_KEY") of
         false ->
-            error({missing_env, "ENTSOE_API_KEY"});
+            case os:getenv("API") of
+                false -> error({missing_env, "ENTSOE_API_KEY"});
+                "" -> error({empty_env, "API"});
+                Api -> {api_key, Api}
+            end;
         "" ->
             error({empty_env, "ENTSOE_API_KEY"});
         ApiKey ->
             {api_key, ApiKey}
     end.
 
+%% Backwards-compatible API: fetch the ENTSO-E page and return HTTP status/body.
 getpage(Date) ->
     {api_key, Api} = apikey(),
     application:ensure_all_started(ssl),
     application:ensure_all_started(inets),
+    Url = entsoe_url(Date, Api),
+    {ok, {{_, Code, _}, _, CodeBase}} = httpc:request(Url),
+    {ok, Code, CodeBase}.
+
+%% Fetch one UTC day into /var/www/.../var/YYYY/MM/DD/entso.xml.
+%% entso_st.txt and entso_end.txt are intentionally not written anymore;
+%% their values are returned as Erlang data in the metadata map.
+fetch_day(Day) ->
+    DayDir = filename:join([?PX | string:split(Day, "-", all)]),
+    Out = filename:join(DayDir, "entso.xml"),
+    Tmp = filename:join(?VOL, "." ++ Day ++ ".xml.tmp"),
+    ok = filelib:ensure_dir(filename:join(DayDir, "dummy")),
+    case existing_xml_metadata(Out) of
+        {ok, Metadata} ->
+            log("get_entso_xml: ~s on jo olemassa ja sisältää start-kentän, ei haeta", [Out]),
+            {ok, Metadata};
+        missing ->
+            fetch_day(Day, Out, Tmp);
+        invalid ->
+            log("get_entso_xml: ~s on olemassa mutta start-kenttä puuttuu, haetaan uudestaan", [Out]),
+            fetch_day(Day, Out, Tmp)
+    end.
+
+fetch_day(Day, Out, Tmp) ->
+    {api_key, Api} = apikey(),
+    application:ensure_all_started(ssl),
+    application:ensure_all_started(inets),
+    Url = entsoe_url(Day, Api),
+    Request = {Url, []},
+    HttpOptions = [{connect_timeout, 30000}, {timeout, 240000}],
+    Options = [{body_format, binary}],
+    Result = httpc:request(get, Request, HttpOptions, Options),
+    case Result of
+        {ok, {{_, 200, _}, _Headers, Body}} when byte_size(Body) > 0 ->
+            ok = file:write_file(Tmp, Body),
+            case existing_xml_metadata(Tmp) of
+                {ok, Metadata0} ->
+                    ok = file:rename(Tmp, Out),
+                    Metadata = Metadata0#{file => Out},
+                    log("get_entso_xml: haettu ~s", [Out]),
+                    {ok, Metadata};
+                _ ->
+                    _ = file:delete(Tmp),
+                    log("get_entso_xml: ERROR day=~s ladattu XML ei sisällä start-kenttää", [Day]),
+                    {error, missing_start}
+            end;
+        {ok, {{_, Code, _}, _Headers, _Body}} ->
+            _ = file:delete(Tmp),
+            log("get_entso_xml: ERROR day=~s HTTP=~p", [Day, Code]),
+            {error, {http_status, Code}};
+        {error, Reason} ->
+            _ = file:delete(Tmp),
+            log("get_entso_xml: ERROR day=~s ~p", [Day, Reason]),
+            {error, Reason}
+    end.
+
+existing_xml_metadata(Out) ->
+    case file:read_file_info(Out) of
+        {ok, #file_info{size = Size}} when Size > 0 ->
+            xml_metadata(Out);
+        {ok, _} ->
+            invalid;
+        {error, enoent} ->
+            missing;
+        {error, _} ->
+            missing
+    end.
+
+xml_metadata(File) ->
+    try
+        {Doc, _} = xmerl_scan:file(File),
+        Starts = xmerl_xpath:string("//*[local-name()='timeInterval']/*[local-name()='start']/text()", Doc),
+        Ends = xmerl_xpath:string("//*[local-name()='timeInterval']/*[local-name()='end']/text()", Doc),
+        StartValues = text_values(Starts),
+        EndValues = text_values(Ends),
+        case StartValues of
+            [] ->
+                invalid;
+            _ ->
+                {ok, #{file => File, start => StartValues, 'end' => EndValues}}
+        end
+    catch
+        _:_ -> invalid
+    end.
+
+text_values(TextNodes) ->
+    [unicode:characters_to_list(Value) || #xmlText{value = Value} <- TextNodes].
+
+entsoe_url(Date, Api) ->
     Interval = Date ++ "T09%3A00Z%2F" ++ Date ++ "T15%3A00Z",
     Query = "documentType=A44&in_Domain=" ++ ?DOM ++
         "&out_Domain=" ++ ?DOM ++
         "&timeInterval=" ++ Interval ++
         "&securityToken=" ++ Api,
-    Url = "https://" ++ ?HOST ++ "/api?" ++ Query,
+    "https://" ++ ?HOST ++ "/api?" ++ Query.
 
-    {ok, {{_, Code, _}, _, CodeBase}} = httpc:request(Url),
-    {ok, Code, CodeBase}.
+log(Format, Args) ->
+    Line = io_lib:format("~s " ++ Format ++ "~n", [timestamp() | Args]),
+    _ = filelib:ensure_dir(?LOG),
+    _ = file:write_file(?LOG, Line, [append]),
+    logger:info(Format, Args),
+    ok.
+
+timestamp() ->
+    {{Y, M, D}, {H, Min, S}} = calendar:local_time(),
+    lists:flatten(io_lib:format("~4..0B-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B", [Y, M, D, H, Min, S])).
--- src/quarter_worker.erl
+++ src/quarter_worker.erl
@@ -4,6 +4,8 @@
 -export([start_link/0, do_work/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-record(entso_xml, {day, file, start = [], 'end' = []}).
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -13,12 +15,14 @@ do_work() ->
 
 %% gen_server callbacks
 init([]) ->
-    {ok, #{}}.
+    {ok, #{entso_xml => #{}}}.
 
 handle_cast(do_work, State) ->
-    Day = eutils:tomorrow_date(),
-    logger:info("qw ~p day:~p", [calendar:local_time(), Day]),
-    {noreply, State};
+    TodayUtc = eutils:today_utc_string(),
+    FetchDays = fetch_days(TodayUtc, eutils:local_hour()),
+    logger:info("qw ~p fetch_days:~p", [calendar:local_time(), FetchDays]),
+    NewState = lists:foldl(fun fetch_and_store/2, State, FetchDays),
+    {noreply, NewState};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -34,3 +38,24 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+fetch_days(TodayUtc, HourLocal) when HourLocal >= 15 ->
+    [TodayUtc, eutils:tomorrow_utc_string()];
+fetch_days(TodayUtc, _HourLocal) ->
+    [TodayUtc].
+
+fetch_and_store(Day, State) ->
+    case kurl:fetch_day(Day) of
+        {ok, Metadata} ->
+            EntsoXml = #entso_xml{
+                day = Day,
+                file = maps:get(file, Metadata),
+                start = maps:get(start, Metadata, []),
+                'end' = maps:get('end', Metadata, [])
+            },
+            EntsoByDay0 = maps:get(entso_xml, State, #{}),
+            State#{entso_xml => EntsoByDay0#{Day => EntsoXml}};
+        {error, Reason} ->
+            logger:error("get_entso_xml: day=~s failed: ~p", [Day, Reason]),
+            State
+    end.
